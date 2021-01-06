747d3388d0e745a0751784cb25b3535def8ba264
Cameron Moore <cam@cameronlpmoore.com>
2021-01-06 15:10:41 -0800

    configures Stripe checkout on the server

--- stripe-js/client/src/index.js
+++ stripe-js/client/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+// Asynchronously load Stripe and make it available in the application
+export const stripePromise = loadStripe(
+  "pk_test_51I6OvTEjROIhSv1mvAXypKVvoJuVxM9cniUTvJthufUOcDTLpBpKm60k8pVH8uZSaz1s380XL1jFrHbAUtMUo2F600pxJiN2O6"
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
